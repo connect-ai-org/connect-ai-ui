8fbb30bff6d3091d9a740675a2f4db703b7a332f
Andrew <38687378+andrew1044@users.noreply.github.com>
2023-08-09 00:20:27 +1000

    content update
    for home page and design page

--- src/app/modules/home/constants/landing-page-content.constant.ts
+++ src/app/modules/home/constants/landing-page-content.constant.ts
@@ -2,7 +2,7 @@ const SERVICES = [
   {
     id: 1,
     name: 'Design',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eveniet enim alias eos voluptatem corporis consequuntur error porro aliquid repellendus voluptate incidunt quis, atque sapiente cum eius. Hic, alias nobis.',
+    description: 'Web design is the process of planning and organising website content for the purpose of creating a user-friendly, and functional online platform that introduces information about businesses, promotes products or services and engages visitors. From branding to web design and software engineering, Connect AI offers a wide range of design services.',
     imageUrl: 'assets/images/services/design.jpg',
     imageDescription: 'Design Service',
     routing: '/design',
@@ -10,7 +10,7 @@ const SERVICES = [
   {
     id: 2,
     name: 'Digital',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eveniet enim alias eos voluptatem corporis consequuntur error porro aliquid repellendus voluptate incidunt quis, atque sapiente cum eius. Hic, alias nobis.',
+    description: 'Digital marketing refers to the strategic use of various online channels and platforms to promote products, services, or brands and engage with target audiences. Connect AI offers services in Search Engine Optimization (SEO), Social Media Marketing, Pay-Per-Click Advertising (Google Ads) and more.',
     imageUrl: 'assets/images/services/digital.jpg',
     imageDescription: 'Digital Service',
     routing: '/digital',
@@ -18,7 +18,7 @@ const SERVICES = [
   {
     id: 3,
     name: 'Marketing',
-    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore eveniet enim alias eos voluptatem corporis consequuntur error porro aliquid repellendus voluptate incidunt quis, atque sapiente cum eius. Hic, alias nobis.',
+    description: 'Marketing involves the methods and plans that a business can employ to showcase their products, services, or brand to possible customers. When there\'s less competition and you have more partners, customers, and brand recognition, using marketing can help increase your sales. As more customers become aware of your business and have a favorable opinion of it, they\'re more inclined to buy from you.',
     imageUrl: 'assets/images/services/marketing.jpg',
     imageDescription: 'Marketing Service',
     routing: '/marketing',
